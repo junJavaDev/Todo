923528a13739af62b968e80f12b4724a6d129c30
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-12-11 23:40:40 +0300

    add categories display in left toolbar

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,15 +1,20 @@
 import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from "../../service/data-handler.service";
+import {Category} from "../../model/Category";
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
-export class CategoriesComponent implements OnInit{
-  constructor() {
+export class CategoriesComponent implements OnInit {
+  categories: Category[];
+
+  constructor(private dataHandler: DataHandlerService) {
+    this.categories = this.dataHandler.getCategories();
   }
 
+  // Метод вызывается автоматически после инициализации компонента
   ngOnInit() {
   }
-
 }
